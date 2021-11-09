7d54c74417f1e958688b53ab84ba711291c63f5e
adele <adele@LAPTOP-FN50S4DB.cust.communityfibre.co.uk>
2021-11-09 18:54:39 +0000

    Main = series in parallel

--- main.s
+++ main.s
@@ -1,18 +1,22 @@
-	#include <xc.inc>
+#include <xc.inc>
 
 psect	code, abs
 	
-main:
-	org	0x0
-	goto	start
-
-	org	0x100		    ; Main code starts here at address 0x100
-start:
-	movlw 	0x0
-	movwf	TRISB, A	    ; Port C all outputs
-	bra 	test
-loop:
-	movff 	0x06, PORTB
-	incf 	0x06, W, A
-test:
-	
+SPI_MasterInit:	    ;Set Clock edge to negative
+    bcf CKE2	    ; CKE bit in SSP2STAT
+    ; MSSP enable; CKP = 1, SPI master, clock = Fosc / 64 (1MHz)
+    movlw   (SSP2CON1_SSPEN_MASK) | (SSP2CON1_CKP_MASK) | (SSP2CON1_SSPM1_MASK)
+    movwf  SSP2CON1, A
+    ; SDO2 output ;  SCK2 output
+    bcf TRISD, PORTD_SDO2_POSN, A  ; SDO2 output
+    bcf TRISD, PORTD_SDK2_POSN, A  ; SDK2 output
+    return
+    
+SPI_MasterTransmit: ;Start transmission of data (held in W)
+    movwf SSP2BUF, A  ;write data to output buffer
+   
+ Wait_Transmit:   ;Wait for transmission to complete
+	btfss	SSP2IF	;check interrupt flag to see if data has been sent
+	bra Wait_Transmit
+	bcf SSP2IF  ;clear interrupt flag
+	return

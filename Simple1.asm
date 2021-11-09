2ddc69c3726ef442486d19dda70385317dce2d8c
adele <adele@LAPTOP-FN50S4DB>
2021-11-09 10:22:56 +0000

    Test commit to learn how to use GitHub, laqt version

--- Simple1.asm
+++ Simple1.asm
@@ -1,21 +1,29 @@
 	#include p18f87k22.inc
 	
-	code
+psect	code, abs
+counter:
 	org 0x0
 	goto	start
 	
 	org 0x100		    ; Main code starts here at address 0x100
 
-start
+start:
 	movlw 	0x0
-	movwf	TRISB, ACCESS	    ; Port C all outputs
+	movwf	TRISB, A	    ; Port C all outputs
 	bra 	test
-loop	movff 	0x06, PORTB
-	incf 	0x06, W, ACCESS
-test	movwf	0x06, ACCESS	    ; Test for end of loop condition
+	
+loop:	movff 	0x06, PORTB
+	incf 	0x06, W, A
+	call delay
+	
+	
+test: 	movwf	0x06, A	    ; Test for end of loop condition
 	movlw 	0x63
-	cpfsgt 	0x06, ACCESS
+	cpfsgt 	0x06, A
 	bra 	loop		    ; Not yet finished goto start of loop again
 	goto 	0x0		    ; Re-run program from start
 	
+delay: decfsz 0x06, A
+       bra delay
+       
 	end

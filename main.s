9bc0632bcbab8540411ecb4fcef7002cb689ff16
alj19 <alj19@skbl-422-015.ph.ic.ac.uk>
2021-11-15 11:25:50 +0000

    Line LCD

--- main.s
+++ main.s
@@ -1,7 +1,7 @@
 #include <xc.inc>
 
 extrn	UART_Setup, UART_Transmit_Message  ; external subroutines
-extrn	LCD_Setup, LCD_Write_Message
+extrn	LCD_Setup, LCD_Write_Message, LCD_Clear
 	
 psect	udata_acs   ; reserve data space in access ram
 counter:    ds 1    ; reserve one byte for a counter variable
@@ -9,7 +9,6 @@ delay_count:ds 1    ; reserve one byte for counter in the delay routine
     
 psect	udata_bank4 ; reserve data anywhere in RAM (here at 0x400)
 myArray:    ds 0x80 ; reserve 128 bytes for message data
-myArray2:   ds 0x80 ;added
 
 psect	data    
 	; ******* myTable, data in programme memory, and its length *****
@@ -19,11 +18,6 @@ myTable:
 	myTable_l   EQU	13	; length of data
 	align	2
 
-ClearTable: ;added
-    db	'C',0x0a
-					; message, plus carriage return
-	ClearTable_l   EQU	1	; length of data
-	align	2
 
 psect	code, abs	
 rst: 	org 0x0
@@ -46,7 +40,6 @@ start: 	lfsr	0, myArray	; Load FSR0 with address in RAM
 	movwf	TBLPTRL, A		; load low byte to TBLPTRL
 	movlw	myTable_l	; bytes to read
 	movwf 	counter, A		; our counter register
-	call	Clear   ;added
 	
 loop: 	tblrd*+			; one byte from PM to TABLAT, increment TBLPRT
 	movff	TABLAT, POSTINC0; move data from TABLAT to (FSR0), inc FSR0	
@@ -61,6 +54,7 @@ loop: 	tblrd*+			; one byte from PM to TABLAT, increment TBLPRT
 	addlw	0xff		; don't send the final carriage return to LCD
 	lfsr	2, myArray
 	call	LCD_Write_Message
+	call	LCD_Clear
 
 	goto	$		; goto current line in code
 	; a delay subroutine if you need one, times around loop in delay_count
@@ -68,25 +62,8 @@ delay:	decfsz	delay_count, A	; decrement until zero
 	bra	delay
 	return
 
-Clear: ;added
-	lfsr	0, myArray2	; Load FSR0 with address in RAM	
-	movlw	low highword(ClearTable)	; address of data in PM
-	movwf	TBLPTRU, A		; load upper bits to TBLPTRU
-	movlw	high(ClearTable)	; address of data in PM
-	movwf	TBLPTRH, A		; load high byte to TBLPTRH
-	movlw	low(ClearTable)	; address of data in PM
-	movwf	TBLPTRL, A		; load low byte to TBLPTRL
-	movlw	ClearTable_l	; bytes to read
-	movwf 	counter, A
-	movlw	ClearTable_l	; output message to UART
-	lfsr	2, myArray2
-	call	UART_Transmit_Message
+clear:
+	call    LCD_Clear   ;added
 
-	movlw	ClearTable_l	; output message to LCD
-	addlw	0xff		; don't send the final carriage return to LCD
-	lfsr	2, myArray2
-	call	LCD_Write_Message
-	
-	return
 
 	end	rst

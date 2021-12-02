6facfe93d458c1403f29e9e48eca19ceb2e68700
alj19 <alj19@skbl-422-015.ph.ic.ac.uk>
2021-12-02 10:53:25 +0000

    New Branch, ready to work now

--- main.s
+++ main.s
@@ -1,35 +1,47 @@
     #include <xc.inc>
 
-    psect	code, abs
+psect	code, abs
 
-    goto Start
-	
-Start:	
-    movlw	0x00	;all bits in
-    movwf	TRISD, A ;port D direction Register
-    movff	0x00, PORTD	
-    call	SPI_MasterInit
-    call	SPI_MasterTransmit
-		
+goto Start
 
-SPI_MasterInit:	    ;Set Clock edge to negative
-    bcf CKE2	    ; CKE bit in SSP2STAT
-    ; MSSP enable; CKP = 1, SPI master, clock = Fosc / 64 (1MHz)
-    movlw   (SSP2CON1_SSPEN_MASK) | (SSP2CON1_CKP_MASK) | (SSP2CON1_SSPM1_MASK)
-    movwf  SSP2CON1, A
-    ; SDO2 output ;  SCK2 output
-    bcf TRISD, PORTD_SDO2_POSN, A  ; SDO2 output
-    bcf TRISD, PORTD_SCK2_POSN, A  ; SDK2 output
-    return
+extrn	sensor_setup
     
-SPI_MasterTransmit: ;Start transmission of data (held in W)
-    movwf SSP2BUF, A  ;write data to output buffer
-   
- Wait_Transmit:   ;Wait for transmission to complete
-	btfss	SSP2IF	;check interrupt flag to see if data has been sent
-	bra Wait_Transmit
-	bcf SSP2IF  ;clear interrupt flag
-	return
+psect	code, abs	
+
+start: 
+    call sensor_setup
+    
+    
+    
+    
+       
+;Start:	
+;    movlw	0x00	;all bits in
+;    movwf	TRISD, A ;port D direction Register
+;    movff	0x00, PORTD	
+;    call	SPI_MasterInit
+;    call	SPI_MasterTransmit
+;		
+;
+;SPI_MasterInit:	    ;Set Clock edge to negative
+;    bcf CKE2	    ; CKE bit in SSP2STAT
+;    ; MSSP enable; CKP = 1, SPI master, clock = Fosc / 64 (1MHz)
+;    movlw   (SSP2CON1_SSPEN_MASK) | (SSP2CON1_CKP_MASK) | (SSP2CON1_SSPM1_MASK)
+;    movwf  SSP2CON1, A
+;    ; SDO2 output ;  SCK2 output
+;    bcf TRISD, PORTD_SDO2_POSN, A  ; SDO2 output
+;    bcf TRISD, PORTD_SCK2_POSN, A  ; SDK2 output
+;    return
+;    
+;SPI_MasterTransmit: ;Start transmission of data (held in W)
+;    movwf SSP2BUF, A  ;write data to output buffer
+;   
+; Wait_Transmit:   ;Wait for transmission to complete
+;	btfss	SSP2IF	;check interrupt flag to see if data has been sent
+;	bra Wait_Transmit
+;	bcf SSP2IF  ;clear interrupt flag
+;	return
+;	
+
 	
-	
-;commit test
+

4669c42ba75adb46bd42db81f96861a125117e25
alj19 <alj19@skbl-422-015.ph.ic.ac.uk>
2021-12-06 14:24:01 +0000

    Working on LCD Problem with adding end at the end of the code

--- main.s
+++ main.s
@@ -2,21 +2,19 @@
 
  psect code, abs
 
- goto start
-
  
-extrn sensor_setup, portcsetup, loop, sensorread
-extrn UART_Setup, UART_Transmit_Message  ; external uart subroutines
-extrn LCD_Setup, LCD_Write_Message ; external LCD subroutines
-extrn ADC_Setup, ADC_Read               ; external ADC subroutines   
-extrn initiate              ; external timer subroutine
+extrn sensor_setup, portcsetup, loopsensor, sensorread	    ; external heart rate click
+extrn UART_Setup, UART_Transmit_Message			    ; external uart subroutines
+extrn LCD_Setup, LCD_Write_Message, LCD_Write_Instruction   ; external LCD subroutines
+extrn ADC_Setup, ADC_Read				    ; external ADC subroutines   
+extrn initiate						    ; external timer subroutine
 
-psect udata_acs   ; reserve data space in access ram
+psect udata_acs	    ; reserve data space in access ram
 counter:    ds 1    ; reserve one byte for a counter variable
 delay_count:ds 1    ; reserve one byte for counter in the delay routine
 
 
-psect udata_bank4 ; reserve data anywhere in RAM (here at 0x400)
+psect udata_bank4   ; reserve data anywhere in RAM (here at 0x400)
 myArray:    ds 0x80 ; reserve 128 bytes for message data
 
 psect data   
@@ -41,52 +39,70 @@ myTable3:
            align  2
 
 psect code, abs   
-rst:     org 0x0
-goto  setup
+rst:     
+	org 0x0
+	goto  setup
 
            ; ******* Programme FLASH read Setup Code ***********************
-setup:          bcf     CFGS ; point to Flash program memory 
+	  
+setup:     bcf     CFGS ; point to Flash program memory 
            bsf     EEPGD         ; access Flash program memory
-	   call initiate
-	   call     UART_Setup         ; setup UART
-           call     LCD_Setup  ; setup UART
-           call     ADC_Setup ; setup ADC
-           call    portcsetup
-           call    sensor_setup
-           call    loop
-           call    sensorread
-	   goto  start   
-   
-
+	   call	   initiate
+	   call    UART_Setup       ; setup UART
+           call    LCD_Setup	    ; setup LCD: PORTB
+           call    ADC_Setup	    ; setup ADC: PORTE
+           call    portcsetup	    ; clear everything PORTC
+           call    sensor_setup	    ; setup heart rate click: PORTC
+	   goto	   start 
+    
+	   ;********* Main Programme *************
+clear:
+	movlw	00000001B
+	call	LCD_Transmit_Instruction
+	return
+	   
+	   
 start:
-           lfsr     0, myArray ; Load FSR0 with address in RAM    
+           ;call    sensorread		    ;to use heart rate click
+	   ;call    loopsensor
+	   
+           lfsr		 0, myArray			; Load FSR0 with address in RAM    
            ; put if loop here to change the code depending on the frequency !!
-           movlw         low highword(myTable)          ; address of data in PM
-           movwf         TBLPTRU, A           ; load upper bits to TBLPTRU
-           movlw         high(myTable)      ; address of data in PM
-           movwf         TBLPTRH, A           ; load high byte to TBLPTRH
-           movlw         low(myTable)       ; address of data in PM
-           movwf         TBLPTRL, A            ; load low byte to TBLPTRL
-           movlw         myTable_l   ; bytes to read
+           movlw         low highword(myTable)  ; address of data in PM
+           movwf         TBLPTRU, A		; load upper bits to TBLPTRU
+           movlw         high(myTable)		; address of data in PM
+           movwf         TBLPTRH, A		; load high byte to TBLPTRH
+           movlw         low(myTable)		; address of data in PM
+           movwf         TBLPTRL, A		; load low byte to TBLPTRL
+           movlw         myTable_l		; bytes to read
            movwf         counter, A             ; our counter register
  
-;loop:	   tblrd*+                             ; one byte from PM to TABLAT, increment TBLPRT
-;           movff TABLAT, POSTINC0; move data from TABLAT to (FSR0), inc FSR0   
-;           decfsz          counter, A             ; count down to zero
-;           bra    loop             ; keep going until finished
-;           movlw         myTable_l   ; output message to UART
-;           lfsr     2, myArray
-;           call     UART_Transmit_Message
-;           movlw         myTable_l-1          ; output message to LCD
-;                                          ; don't send the final carriage return to LCD
-;           lfsr     2, myArray
-;           call     LCD_Write_Message
-
-   
+loop:	   tblrd*+				; one byte from PM to TABLAT, increment TBLPRT
+           movff	   TABLAT, POSTINC0	; move data from TABLAT to (FSR0), inc FSR0   
+           decfsz          counter, A           ; count down to zero
+           bra		   loop			; keep going until finished
+           movlw	   myTable_l		; output message to UART
+           lfsr		   2, myArray
+           call		   UART_Transmit_Message
+           movlw	   myTable_l-1		; output message to LCD
+						; don't send the final carriage return to LCD
+           lfsr		   2, myArray
+           call		   LCD_Write_Message
+	   goto		   $
+ 
+	     
+;loop: 	tblrd*+			; one byte from PM to TABLAT, increment TBLPRT
+;	movff	TABLAT, POSTINC0; move data from TABLAT to (FSR0), inc FSR0	
+;	decfsz	counter, A		; count down to zero
+;	bra	loop		; keep going until finished
+;		
+;	movlw	myTable_l	; output message to UART
+;	lfsr	2, myArray
+;	call	UART_Transmit_Message
+;
+;	goto	$		; goto current line in code
 
  
-;         ; ******* Main programme ****************************************
-
  
 
 ;measure_loop:
